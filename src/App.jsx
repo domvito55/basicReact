ee79ecc70e1acb424ce28716084374154b827d1d
domvito55 <domvito55@gmail.com>
2024-06-14 00:40:07 +0000

    Something Went Wrong

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Welcome from "./components/Welcome";
 import ListCast from "./components/ListCast";
 import Modals from "./components/Modals";
+import Nav from "./components/Nav";
 
 function App() {
-  const name = "StarGazers";
   const [memberInfo, setMemberInfo] = useState(null);
+  const [cast, setCast] = useState([]);
+
+  async function fetchCast() {
+    const response = await fetch("cast.json");
+    setCast(await response.json());
+  }
+
+  useEffect(() => {
+    fetchCast();
+  });
+
   return (
-    <div className="container">
-      <hgroup>
-        <img src="images/group.svg" alt="StarGazers Group" />
-        <Welcome name="StarGazers" />
-        <p>
-          Members of an <b>intergalactic alliance</b>
-          <br />
-          paving the way for peace and benevolence among all species. They are
-          known for their enthusiasm for science, for their love of fun, and
-          their dedication to education.
-        </p>
-        <ListCast
-          onChoice={(info) => {
-            setMemberInfo(info);
-          }}
-        />
-        {memberInfo && (
-          <Modals
-            member={memberInfo}
-            //You cannot simple use setMemberInfo(null) here because it will not work
-            //The reason is that the function is called in the child component and the state is in the parent component
-            //So, you need to pass a function to the child component to change the state in the parent component
-            handleClose={() => {
-              setMemberInfo(null);
+    <>
+      <Nav
+        cast={cast}
+        onChoice={(info) => {
+          setMemberInfo(info);
+        }}
+      />
+      <div className="container">
+        <hgroup>
+          <img src="images/group.svg" alt="StarGazers Group" />
+          <Welcome name="StarGazers" />
+          <p>
+            Members of an <b>intergalactic alliance</b>
+            <br />
+            paving the way for peace and benevolence among all species. They are
+            known for their enthusiasm for science, for their love of fun, and
+            their dedication to education.
+          </p>
+          <ListCast
+            cast={cast}
+            onChoice={(info) => {
+              setMemberInfo(info);
             }}
           />
-        )}
-      </hgroup>
-    </div>
+          {memberInfo && (
+            <Modals
+              member={memberInfo}
+              //You cannot simple use setMemberInfo(null) here because it will not work
+              //The reason is that the function is called in the child component and the state is in the parent component
+              //So, you need to pass a function to the child component to change the state in the parent component
+              handleClose={() => {
+                setMemberInfo(null);
+              }}
+            />
+          )}
+        </hgroup>
+      </div>
+    </>
   );
 }
 export default App;
